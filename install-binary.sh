a88342625b7e32c784ff1384a4a4139d12992ea7
Dan Wendorf <dan.wendorf@opendoor.com>
2018-06-06 11:38:02 -0700

    Install from opendoor-labs org

--- install-binary.sh
+++ install-binary.sh
@@ -3,7 +3,7 @@
 # Shamelessly copied from https://github.com/technosophos/helm-template
 
 PROJECT_NAME="helm-diff"
-PROJECT_GH="databus23/$PROJECT_NAME"
+PROJECT_GH="opendoor-labs/$PROJECT_NAME"
 
 : ${HELM_PLUGIN_PATH:="$(helm home)/plugins/helm-diff"}
 
